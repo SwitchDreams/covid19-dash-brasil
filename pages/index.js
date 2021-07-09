9d159c386aadaca95c55102d634331ff05c2e55f
lucasazuma <azumalucas@gmail.com>
2021-07-09 12:03:03 -0300

    data collection working

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,58 @@
 import React from 'react';
 import {Mercator, Graticule} from '@visx/geo';
 import * as topojson from 'topojson-client';
-import topology from '../brazil.json';
+// import topology from '../brazil.json';
 
-export default function Home() {
-
+export default function Home({data}) {
   // const topology = fetch("https://raw.githubusercontent.com/fititnt/gis-dataset-brasil/master/uf/topojson/uf.json").then(d => d.json())
 
   // const world = topojson.feature(topology, topology.objects.units)
 
+
   return (
     <div>
-
-      <svg width={500} height={500}>
-        <Mercator data={topology.features}/>
-        {mercator => (
-          <g>
-            <Graticule graticule={g => mercator.path(g) || ''} stroke="rgba(33,33,33,0.05)"/>
-            {mercator.features.map(({feature, path}, i) => (
-              <path
-                key={`map-feature-${i}`}
-                d={path || ''}
-                fill={'#333'}
-                strokeWidth={0.5}
-                onClick={() => {
-                  if (events) alert(`Clicked: ${feature.properties.name} (${feature.id})`);
-                }}
-              />
-            ))}
-          </g>
-        )}
-      </svg>
-
+        <p>{JSON.stringify(data)}</p>
     </div>
   )
+}
+
+function csvJSON(csv){
+
+    let lines = csv.split("\n");
+
+    let result = [];
+
+    let headers=lines[0].split(",");
+
+    for(let i=1;i<lines.length;i++){
+
+        let obj = {};
+        let currentline=lines[i].split(",");
+
+        for(let j=0;j<headers.length;j++){
+            if(typeof currentline[j] === 'undefined'){
+                obj[headers[j]] = null
+            }else{
+                obj[headers[j]] = currentline[j];
+            }
+        }
+        result.push(obj);
+    }
+
+    //return result; //JavaScript object
+    return result; //JSON
+}
+
+export async function getStaticProps(context) {
+    const res = await fetch(`https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv`)
+    const data = await csvJSON(await res.text())
+    console.log(data[0])
+    if (!data) {
+        return {
+            notFound: true,
+        }
+    }
+    return {
+        props: { data: data } // will be passed to the page component as props
+    }
 }
